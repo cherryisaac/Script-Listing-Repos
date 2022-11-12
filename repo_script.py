f5ebb1232e1d05d2cb5b76f7265e97716be8a32e
icherry739 <106172566+icherry739@users.noreply.github.com>
2022-11-12 11:36:46 -0500

    Update repo_script.py
    small fix

--- repo_script.py
+++ repo_script.py
@@ -1,7 +1,7 @@
 import requests
 import csv
 
-# github_token = "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"
+# github_token = "YOUR_AUTH_TOKEN"
 
 # Github auth token required to run file/url more than 60 times per hour otherwise comment this out along with headers=headers
 print("Please input auth token:")
